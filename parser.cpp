2eca4a61e98ff0c3b908485ae12c19f77ff6b218
Centronias <work@centronias.com>
2015-02-11 15:55:16 -0800

    Modified Grammar

--- parser.cpp
+++ parser.cpp
@@ -31,277 +31,257 @@ Parser::init()
 	printf("Initializing parser.\n");
 
 	// Create the terminal symbols
-	Terminal*	oParen	= new Terminal(Token::Paren, "(");
-	Terminal*	cParen	= new Terminal(Token::Paren, ")");
-	Terminal*	assignment	= new Terminal(Token::Operator, ":=");
-	Terminal*	identifier	= new Terminal(Token::Identifier);
+	Terminal*	open	= new Terminal(Token::Paren, "(");
+	Terminal*	close	= new Terminal(Token::Paren, ")");
+	Terminal*	assign	= new Terminal(Token::Operator, ":=");
+	Terminal*	iden	= new Terminal(Token::Identifier);
 
-	Terminal*	add	= new Terminal(Token::Operator, "+");
 	Terminal*	sub	= new Terminal(Token::Operator, "-");
-	Terminal*	mul	= new Terminal(Token::Operator, "*");
-	Terminal*	div	= new Terminal(Token::Operator, "/");
-	Terminal*	mod	= new Terminal(Token::Operator, "%");
-	Terminal*	exp	= new Terminal(Token::Operator, "^");
-	Terminal*	equ	= new Terminal(Token::Operator, "=");
-	Terminal*	grt	= new Terminal(Token::Operator, ">");
-	Terminal*	gte	= new Terminal(Token::Operator, ">=");
-	Terminal*	lst	= new Terminal(Token::Operator, "<");
-	Terminal*	lte	= new Terminal(Token::Operator, "<=");
-	Terminal*	nte	= new Terminal(Token::Operator, "!=");
-	Terminal*	boo	= new Terminal(Token::Operator, "or");
-	Terminal*	boa	= new Terminal(Token::Operator, "and");
-	Terminal*	bon	= new Terminal(Token::Operator, "not");
-	Terminal*	sin	= new Terminal(Token::Operator, "sin");
-	Terminal*	cos	= new Terminal(Token::Operator, "cos");
-	Terminal*	tan	= new Terminal(Token::Operator, "tan");
 
-	Terminal*	strConst	= new Terminal(Token::StrConst);
-	Terminal*	intConst	= new Terminal(Token::IntConst);
-	Terminal*	realConst	= new Terminal(Token::RealConst);
-	Terminal*	boolConst	= new Terminal(Token::BoolConst);
+	comVector<Terminal*>	binOps;
+	binOps.append(new Terminal(Token::Operator, "+"));
+	binOps.append(new Terminal(Token::Operator, "*"));
+	binOps.append(new Terminal(Token::Operator, "/"));
+	binOps.append(new Terminal(Token::Operator, "%"));
+	binOps.append(new Terminal(Token::Operator, "^"));
+	binOps.append(new Terminal(Token::Operator, "="));
+	binOps.append(new Terminal(Token::Operator, ">"));
+	binOps.append(new Terminal(Token::Operator, ">="));
+	binOps.append(new Terminal(Token::Operator, "<"));
+	binOps.append(new Terminal(Token::Operator, "<="));
+	binOps.append(new Terminal(Token::Operator, "!="));
+	binOps.append(new Terminal(Token::Operator, "or"));
+	binOps.append(new Terminal(Token::Operator, "and"));
 
-	Terminal*	printStmt	= new Terminal(Token::StmtWord, "stdout");
-	Terminal*	ifStmt		= new Terminal(Token::StmtWord, "if");
-	Terminal*	whileStmt	= new Terminal(Token::StmtWord, "while");
-	Terminal*	letStmt		= new Terminal(Token::Operator, "let");
+	comVector<Terminal*>	unOps;
+	unOps(new Terminal(Token::Operator, "not"));
+	unOps(new Terminal(Token::Operator, "sin"));
+	unOps(new Terminal(Token::Operator, "cos"));
+	unOps(new Terminal(Token::Operator, "tan"));
 
-	Terminal*	primType	= new Terminal(Token::PrimType);
+	Terminal*	constStr	= new Terminal(Token::StrConst);
+	Terminal*	constInt	= new Terminal(Token::IntConst);
+	Terminal*	constReal	= new Terminal(Token::RealConst);
+	Terminal*	constBool	= new Terminal(Token::BoolConst);
+
+	Terminal*	printKey	= new Terminal(Token::StmtWord, "stdout");
+	Terminal*	ifKey		= new Terminal(Token::StmtWord, "if");
+	Terminal*	whileKey	= new Terminal(Token::StmtWord, "while");
+	Terminal*	letKey		= new Terminal(Token::Operator, "let");
+
+	Terminal*	typeName	= new Terminal(Token::PrimType);
+
+	Terminal*	epsilon		= new Terminal(Token::NONE);
 
 
 	// Create the nonterminal symbols
 	m_entrySymbol = new NonTerm("S");
 
-	NonTerm*	expr 		= new NonTerm("EXPR");
-	NonTerm*	oper 		= new NonTerm("OPER");
-	NonTerm*	binOps 		= new NonTerm("BINOPS");
-	NonTerm*	unOps 		= new NonTerm("UNOPS");
-	NonTerm*	consts 		= new NonTerm("CONSTANTS");
-	NonTerm*	stmts 		= new NonTerm("STMTS");
-	NonTerm*	printStmts 	= new NonTerm("PRINTSTMTS");
-	NonTerm*	ifStmts 	= new NonTerm("IFSTMTS");
-	NonTerm*	whileStmts 	= new NonTerm("WHILESTMTS");
-	NonTerm*	letStmts 	= new NonTerm("LETSTMTS");
-	NonTerm*	exprList 	= new NonTerm("EXPRLISTS");
-	NonTerm*	varLists 	= new NonTerm("VARLISTS");
+	NonTerm*	S		= m_entrySymbol;
+	NonTerm*	SPAREN	= new NonTerm("SPAREN");
+	NonTerm*	SP 		= new NonTerm("SP");
+	NonTerm*	EXPR	= new NonTerm("EXPR");
+	NonTerm*	RESR	= new NonTerm("RESR");
+	NonTerm*	OPER	= new NonTerm("OPER");
+	NonTerm*	OPERM	= new NonTerm("OPER_MINUS");
+	NonTerm*	STMTS	= new NonTerm("STMTS");
+	NonTerm*	ELSE	= new NonTerm("ELSE");
+	NonTerm*	ELIST	= new NonTerm("EXPRLIST");
+	NonTerm*	ELISTC	= new NonTerm("EXPRLIST_CAP");
+	NonTerm*	VLIST	= new NonTerm("VARLIST");
+	NonTerm*	VLISTC	= new NonTerm("VARLIST_CAP");
+
+	Production*	prod = NULL;
 
 	// ----------------------------------------------------
 	// S Productions
 	// ----------------------------------------------------
+	// S -> ( SPAREN ) SP
+	prod = new Production;
+	prod->add(open);
+	prod->add(SPAREN);
+	prod->add(close);
+	prod->add(SP);
+	S->addProduction(prod);
 
-	{
-		// S -> ()
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(cParen);
-		m_entrySymbol->addProduction(prod);
-	} {
-		// S -> (S)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(m_entrySymbol);
-		prod->add(cParen);
-		m_entrySymbol->addProduction(prod);
-	} {
-		// S -> EXPR
-		m_entrySymbol->addProduction(expr);
-	} {
-		// S -> ()S
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(cParen);
-		prod->add(m_entrySymbol);
-		m_entrySymbol->addProduction(prod);
-	} {
-		// S -> (S)S
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(m_entrySymbol);
-		prod->add(cParen);
-		prod->add(m_entrySymbol);
-		m_entrySymbol->addProduction(prod);
-	} {
-		// S -> EXPR S
-		Production*	prod = new Production;
-		prod->add(expr);
-		prod->add(m_entrySymbol);
-		m_entrySymbol->addProduction(prod);
-	}
+	// S -> EXPR SP
+	prod = new Production;
+	prod->add(EXPR);
+	prod->add(SP);
+	S->addProduction(prod);
+
+	// ----------------------------------------------------
+	// SPAREN Productions
+	// ----------------------------------------------------
+	// SPAREN -> S
+	SPAREN->addProduction(S);
+
+	// SPAREN -> epsilon
+	SPAREN->addProduction(epsilon);
+
+	// ----------------------------------------------------
+	// SP Productions
+	// ----------------------------------------------------
+	// SP -> S SP
+	prod = new Production;
+	prod->add(S);
+	prod->add(SP);
+	SP->addProduction(prod);
+
+	// SP -> epsilon
+	prod = new Production;
+	prod->add(epsilon);
+	SP->addProduction(prod);
 
 	// ----------------------------------------------------
 	// EXPR Productions
 	// ----------------------------------------------------
-		expr->addProduction(oper);
-		expr->addProduction(stmts);
+	// EXPR -> RESR
+	EXPR->addProduction(RESR);
+
+	// EXPR -> ( STMTS )
+	prod = new Production;
+	prod->add(open);
+	prod->add(STMTS);
+	prod->add(close);
+	EXPR->addProduction(prod);
+
+	// ----------------------------------------------------
+	// RESR Productions
+	// ----------------------------------------------------
+	// RESR -> ( OPER )
+	prod = new Production;
+	prod->add(open);
+	prod->add(OPER);
+	prod->add(close);
+	RESR->addProduction(prod);
+
+	// RESR -> strings | ints | reals | bools | identifiers
+	RESR->addProduction(constStr);
+	RESR->addProduction(constInt);
+	RESR->addProduction(constReal);
+	RESR->addProduction(constBool);
+	RESR->addProduction(iden);
 
 	// ----------------------------------------------------
 	// OPER Productions
 	// ----------------------------------------------------
+	// OPER -> := identifiers RESR
+	prod = new Production;
+	prod->add(assign);
+	prod->add(iden);
+	prod->add(RESR);
+	OPER->addProduction(prod);
 
-	{
-		// OPER -> (:= identifier OPER)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(assignment);
-		prod->add(identifier);
-		prod->add(oper);
-		prod->add(cParen);
-		oper->addProduction(prod);
-	} {
-		// OPER -> (BINOPS OPER OPER)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(binOps);
-		prod->add(oper);
-		prod->add(oper);
-		prod->add(cParen);
-		oper->addProduction(prod);
-	} {
-		// OPER -> (UNOPS OPER)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(unOps);
-		prod->add(oper);
-		prod->add(cParen);
-		oper->addProduction(prod);
-	} {
-		// OPER -> CONSTANTS
-		Production*	prod = new Production;
-		prod->add(consts);
-		oper->addProduction(prod);
-	} {
-		// OPER -> identifier
-		Production*	prod = new Production;
-		prod->add(identifier);
-		oper->addProduction(prod);
+	// OPER -> - RESR OPER_MINUS
+	prod = new Production;
+	prod->add(sub);
+	prod->add(RESR);
+	prod->add(OPERM);
+	OPER->addProduction(prod);
+
+	// <binary operator> RESR RESR
+	for (UINT i = 0; i < binOps.getNumEntries(); i++) {
+		prod = new Production;
+		prod->add(binOps[i]);
+		prod->add(RESR);
+		prod->add(RESR);
+		OPER->addProduction(prod);
+	}
+
+	// <unary operator> RESR
+	for (UINT i = 0; i < unOps.getNumEntries(); i++) {
+		prod = new Production;
+		prod->add(unOps[i]);
+		prod->add(RESR);
+		OPER->addProduction(prod);
 	}
 
 	// ----------------------------------------------------
-	// BINOPS Productions
+	// OPER_MINUS Productions
 	// ----------------------------------------------------
-	binOps->addProduction(add);
-	binOps->addProduction(sub);
-	binOps->addProduction(mul);
-	binOps->addProduction(div);
-	binOps->addProduction(mod);
-	binOps->addProduction(exp);
-	binOps->addProduction(equ);
-	binOps->addProduction(grt);
-	binOps->addProduction(gte);
-	binOps->addProduction(lst);
-	binOps->addProduction(lte);
-	binOps->addProduction(nte);
-	binOps->addProduction(boo);
-	binOps->addProduction(boa);
+	// OPER_MINUS -> RESR
+	OPER_MINUS->addProduction(RESR);
 
-	// ----------------------------------------------------
-	// UNOPS Productions
-	// ----------------------------------------------------
-	unOps->addProduction(sub);
-	unOps->addProduction(bon);
-	unOps->addProduction(sin);
-	unOps->addProduction(cos);
-	unOps->addProduction(tan);
-
-	// ----------------------------------------------------
-	// CONSTANTS Productions
-	// ----------------------------------------------------
-	consts->addProduction(strConst);
-	consts->addProduction(intConst);
-	consts->addProduction(realConst);
-	consts->addProduction(boolConst);
+	// OPER_MINUS -> epsilon
+	OPER_MINUS->addProduction(epsilon);
 
 	// ----------------------------------------------------
 	// STMTS Productions
 	// ----------------------------------------------------
-	stmts->addProduction(ifStmts);
-	stmts->addProduction(whileStmts);
-	stmts->addProduction(letStmts);
-	stmts->addProduction(printStmts);
+	// STMTS -> if EXPR EXPR ELSE
+	prod = new Production;
+	prod->add(ifKey);
+	prod->add(EXPR);
+	prod->add(EXPR);
+	prod->add(ELSE);
+	STMTS->addProduction(prod);
+
+	// STMTS -> while EXPR EXPRLIST
+	prod = new Production;
+	prod->add(whileKey);
+	prod->add(EXPR);
+	prod->add(ELIST);
+	STMTS->addProduction(prod);
+
+	// STMTS -> let ( VARLIST )
+	prod = new Production;
+	prod->add(letKey);
+	prod->add(open);
+	prod->add(VLIST);
+	prod->add(close);
+	STMTS->addProduction(prod);
+
+	// STMTS -> stdout RESR
+	prod = new Production;
+	prod->add(printKey);
+	prod->add(RESR);
+	STMTS->addProduction(prod);
 
 	// ----------------------------------------------------
-	// STMTS Productions
+	// ELSE Productions
 	// ----------------------------------------------------
+	// ELSE -> EXPR
+	ELSE->addProduction(EXPR);
 
-	{
-		// PRINTSTMTS -> (stdout OPER)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(printStmt);
-		prod->add(oper);
-		prod->add(cParen);
-		printStmts->addProduction(prod);
-	} {
-		// IFSTMTS -> (if EXPR EXPR EXPR)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(ifStmt);
-		prod->add(expr);
-		prod->add(expr);
-		prod->add(expr);
-		prod->add(cParen);
-		ifStmts->addProduction(prod);
-	} {
-		// IFSTMTS -> (if EXPR EXPR)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(ifStmt);
-		prod->add(expr);
-		prod->add(expr);
-		prod->add(cParen);
-		ifStmts->addProduction(prod);
-	} {
-		// WHILESTMTS -> (if EXPR EXPRLIST)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(whileStmt);
-		prod->add(expr);
-		prod->add(exprList);
-		prod->add(cParen);
-		whileStmts->addProduction(prod);
-	} {
-		// LETSTMTS -> (LET (VARLIST))
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(letStmt);
-		prod->add(oParen);
-		prod->add(varLists);
-		prod->add(cParen);
-		prod->add(cParen);
-		letStmts->addProduction(prod);
-	}
+	// ELSE -> epsilon
+	ELSE->addProduction(epsilon);
 
 	// ----------------------------------------------------
-	// LIST Productions
+	// EXPRLIST and EXPRLIST_CAP Productions
 	// ----------------------------------------------------
+	// EXPRLIST -> EXPR EXPRLIST_CAP
+	prod = new Production;
+	prod->add(EXPR);
+	prod->add(ELISTC);
+	ELIST->addProduction(prod);
 
-	{
-		// EXPRLIST -> EXPR
-		exprList->addProduction(expr);
-	} {
-		// EXPRLIST -> EXPR EXPRLIST
-		Production*	prod = new Production;
-		prod->add(expr);
-		prod->add(exprList);
-		exprList->addProduction(prod);
-	} {
-		// VARLIST -> (identifier type)
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(identifier);
-		prod->add(primType);
-		prod->add(cParen);
-		varLists->addProduction(prod);
-	} {
-		// VARLIST -> (identifier type) VARLIST
-		Production*	prod = new Production;
-		prod->add(oParen);
-		prod->add(identifier);
-		prod->add(primType);
-		prod->add(cParen);
-		prod->add(varLists);
-		varLists->addProduction(prod);
-	}
+	// EXPRLIST_CAP -> EXPRLIST
+	ELISTC->addProduction(ELIST);
 
+	// EXPRLIST_CAP -> epsilon
+	ELISTC->addProduction(epsilon);
+
+	// ----------------------------------------------------
+	// VARLIST and VARLIST_CAP Productions
+	// ----------------------------------------------------
+	// VARLIST -> (identifier type ) VARLIST_CAP
+	prod = new Production;
+	prod->add(open);
+	prod->add(iden);
+	prod->add(typeName);
+	prod->add(close;
+	prod->add(VLISTC);
+	VLIST->addProduction(prod);
+
+	// VARLIST_CAP -> VARLIST
+	VLISTC->addProduction(VLIST);
+
+	// VARLIST_CAP -> epsilon
+	VLISTC->addProduction(epsilon);
+
+
+	// If we are running in grammar dump mode, dump the grammar and exit.
 	if (Global::isDumpGrammar()) {
 		NonTerm::dumpGrammar();
 		Global::succeed();
@@ -330,11 +310,11 @@ Parser::run()
 // parse()
 // ****************************************************************************
 bool
-Parser::parse(TokenListIter&	iter,
-			  ParseTree*		tree,
-			  UINT&				tokensParsed)
+Parser::parse(TokListIter&	iter,
+			  ParseTree*	tree,
+			  UINT&			tokensParsed)
 {
-	Token*	token = NULL;
+/*	Token*	token = NULL;
 
 	if (tree->isTerminal()) {
 		// If this is a terminal node, something went wrong.
@@ -371,5 +351,5 @@ Parser::parse(TokenListIter&	iter,
 				}
 			}
 		}
-	}
+	}*/
 }
